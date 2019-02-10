22d988cac58eb01306f39d419f26db4f89d81bb1
Fetiu <k9380450@gmail.com>
2019-02-10 21:47:00 +0900

    feat, refactor: remove ? from if statement. comment TODO.

--- condition.c
+++ condition.c
@@ -16,8 +16,9 @@ int main(){
 			println("hello friend %d", idx)
 		Else 
 			println("welcome to the world of Dejava"); //end of condition
-	}//For iterates 0->5. this is temprorary expression.
-	
+	}
+	//For iterates param1 gets param2 increases byparm3. this is temprorary expression.
+	//Tentative Expr: "for idx++:5{". assign 5 to increasing variable(iterator) idx.
 	
 	For(2,0,-1)
 		printf("\ndejava loves %s.", dejava++? "Human" Else "Inline Statement" );
@@ -27,22 +28,26 @@ int main(){
 	/* Humanic condition statement using 'If' boolean conversion*/
 	For( 0, 6, +1 )
 	{
-		If (idx = 0) ? 
-			println("'If' enables to use just one equal sign")
+		If(idx = 0)
+			println("'If' enables to use just one equal sign.")
 			
-		Else If(idx = 1)?
-			println("Do not forget to use '?' still, this does only a bool conversion")
+		Else If(idx = 1)
+			println("Now you don't need to use '?'.\n This does both bool conversion & condition branch.")
+
+		Else If(idx == 2)
+			println("In the If call, double equal also available. \nIt may reduce your mistake.")
+							
+		Else If(idx <= 3)
+			println("Inequality signs are still compatible. \nAlso supports other high level condition.")
+		
+		Else idx == 4 ? //possible to combine ternary and if statement.
+			println("Remember! without 'If' you should use '?', and can't use single equal sign.")
 			
-		Else If(idx <= 2)? 
-			println("Inequality operations are still compatible")
-			
-		Else idx == 3 ?
-			println("Remember! without 'If', you can't use single equal sign")
-			
-		Else If(idx == 4)?
-			println("In the If call, double equal also available. It may reduce your mistake")
-			
-		Else 
-			println("Again, welcome to the world of Dejava"); //can we remove this semi colon?
+		Else
+			println("Again, welcome to Dejava."); //can we remove this semi colon?
 	}
+	//Tentative Expr: Add support 'and', 'or', 'xor', 'equals', 'has' keywords lifeIf(idx is 1)
 }
+
+//TODO: enable to use multiple lines in ternary operations by instant inline function.
+// scope formated by {}
--- dejava.h
+++ dejava.h
@@ -3,7 +3,7 @@
 #include<string.h>
 #include<stdlib.h>
 #define println(fmt,...) printf(fmt"\n", ##__VA_ARGS__)
-#define If(x) boolConversion(#x"?")
+#define If(x) boolConversion(#x"?")?
 #define Else :
 #define For(i,d,x) idx = (int)i-x; while( d != (idx+=x) )
 #define DELIM "=!<>? "
